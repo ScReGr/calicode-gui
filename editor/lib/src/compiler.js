c9ec264eac997c33dfc561d027dae5f216332794
encloinc <jelch2002@gmail.com>
2019-01-24 21:57:38 -0600

    Did a few things

---
+++ editor/lib/src/compiler.js
@@ -0,0 +1,19 @@
+// main {"startScript": [], "objects": []}
+
+// ["type:sysfunc name:name arguments {}", type:func from:higherarchy name:name arguments {}", "func name arg arg arg","type:if statement:statement, script:[]"]
+
+// [object, object, object, object] object = {name:name, objects: objects, scripts: [func, func, func, func], "textures": ["etc1.jpg", "etc2.jpg"]}
+
+// func => {name:name, arguments: {}, script: []}
+
+
+const compile = function(object){
+	//Compiler function
+
+	const main = object.startScript;
+
+
+
+
+
+}
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const fs = require("fs")
 
 
-app.use(express.static(path.join(__dirname, "docs")))
+app.use(express.static(path.join(__dirname, "editor")))
 
 
 app.use(function (req, res, next) {
